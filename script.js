58d7ea0279d5100b49658aee51d3411c20132865
DishaS08 <dishasuryawanshi104@gmail.com>
2025-07-13 20:40:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,22 @@
 const imgs = document.querySelectorAll(".header-slider ul img");
-const prev = document.querySelectorAll(".ctr-prev");
-const next = document.querySelectorAll(".ctr-next");
+const prev = document.querySelector(".ctr-prev");
+const next = document.querySelector(".ctr-next");
 
-var n = 0;
+let n = 0;
 
 function change_Slide() {
-    for (var i = 0; i < imgs.length; i++) {
-        imgs[i].style.display = "none";
-    }
-    imgs[n].style.display = "block";
+  imgs.forEach((img) => (img.style.display = "none"));
+  imgs[n].style.display = "block";
 }
 
 change_Slide();
 
-prev.addEventListener("click", function(e){
-    if(n>0){
-        n--;
-    }else{
-        n = imgs.length-1; 
-    }
-    change_Slide();
-})
+prev.addEventListener("click", () => {
+  n = (n > 0) ? n - 1 : imgs.length - 1;
+  change_Slide();
+});
 
-
-next.addEventListener("click", function(e){
-    if(n<imgs.length - 1){
-        n++;
-    }else{
-        n = 0; 
-    }
-    change_Slide();
-})
+next.addEventListener("click", () => {
+  n = (n < imgs.length - 1) ? n + 1 : 0;
+  change_Slide();
+});
